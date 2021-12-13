d199a9134e8f17bfaab6f2f6c8d5448fce5269d7
Daniel Nehrig <daniel.nehrig@dnehrig.com>
2021-12-13 18:03:03 +0100

    modules
    Signed-off-by: Daniel Nehrig <daniel.nehrig@dnehrig.com>

--- src/day1/test.rs
+++ src/day1/test.rs
@@ -1,15 +1,13 @@
-use std::path;
-
 #[test]
 pub fn solve_a() {
-    let input = super::func::input_generator("./day1/src/test.txt");
+    let input = super::func::input_generator("./src/day1/test.txt");
     let result = super::func::solve_part1(input);
-    assert_eq!(result, 1000);
+    assert_eq!(result, 1722);
 }
 
 #[test]
 pub fn solve_b() {
-    let input = super::func::input_generator("./day1/src/test.txt");
+    let input = super::func::input_generator("./src/day1/test.txt");
     let result = super::func::solve_part2(input);
-    assert_eq!(result, 1000);
+    assert_eq!(result, 1748);
 }
