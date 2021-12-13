9dcfce542976d4ecb3b09d589d31e50cfa4b20ef
Daniel Nehrig <daniel.nehrig@dnehrig.com>
2021-12-13 13:18:51 +0100

    optimize a bit
    Signed-off-by: Daniel Nehrig <daniel.nehrig@dnehrig.com>

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,30 +1,26 @@
-use std::{fs, io::BufRead};
+use std::{fs, io::{BufRead, Result}};
 
-fn main() {
-    let parsed_file: Vec<String> = fs::read("test.txt")
-        .unwrap()
+fn main() -> Result<()> {
+    let parsed_file: Vec<String> = fs::read("test.txt")?
         .lines()
-        .take_while(|e| -> bool { e.as_ref().unwrap().len() > 0 })
-        .map(|e| -> String { e.unwrap() })
-        .collect::<Vec<String>>();
+        .take_while(|e| !e.as_deref().unwrap_or("").is_empty())
+        .collect::<Result<Vec<String>>>()?;
 
     let bit_len: usize = parsed_file[0].len();
-    let mut find_gamma_rate = String::new();
-    (0..bit_len).for_each(|k| {
+    let find_gamma_rate: String = (0..bit_len).map(|k| {
         let nums = parsed_file
-            .clone()
-            .into_iter()
+            .iter()
             .map(|n| n.chars().nth(k).unwrap())
             .collect::<String>();
         let zero = nums.matches("0").count();
         let one = nums.matches("1").count();
 
         if zero > one {
-            find_gamma_rate.extend(["1"]);
+            "1"
         } else {
-            find_gamma_rate.extend(["0"]);
+            "0"
         }
-    });
+    }).collect();
     let gamma_rate = u16::from_str_radix(find_gamma_rate.as_str(), 2).unwrap();
     let epsilon_rate = !gamma_rate & ((1 << find_gamma_rate.len()) - 1);
     let result = gamma_rate as usize * epsilon_rate as usize;
@@ -33,4 +29,5 @@ fn main() {
     println!("Parsed Dec Gamma Rate: {}", gamma_rate);
     println!("Parsed Dec Epsilon Rate: {}", epsilon_rate);
     println!("Result {}", result);
+    Ok(())
 }
