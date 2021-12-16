d955af984c99d81220da5008e1d533d43d30194d
Daniel Nehrig <daniel.nehrig@shop-apotheke.com>
2021-12-16 09:05:39 +0100

    Read Each hex char as u8 for bit operations

--- src/day16/func.rs
+++ src/day16/func.rs
@@ -1,18 +1,17 @@
-use hex::FromHex;
 use std::fs;
 use std::io::BufRead;
 
 pub fn input_generator(file: &str) -> Vec<u8> {
-    Vec::from_hex(fs::read(file)
+    fs::read(file)
         .unwrap()
         .lines()
         .take_while(|n| !n.as_ref().unwrap().is_empty())
         .map(|n| n.unwrap())
-        .collect::<Vec<String>>()[0].to_owned()).unwrap()
+        .collect::<String>().chars().map(|n| u8::from_str_radix(&n.to_string(), 16).unwrap()).collect::<Vec<u8>>()
 }
 
 pub fn solve_a() {
     let input = input_generator("./src/day16/test.txt");
-    println!("{:?}", input);
+    println!("{:b}", input[19]);
     ()
 }
